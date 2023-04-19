512379171d0a2009ff769ff819ca58b46b5fbd9b
Michael Suzzi <msuz.code@gmail.com>
2023-04-19 23:15:31 +1000

    Rewrite TrackTest to use parameterized tests

--- src/test/java/com/msuz/jcuesheet/TrackTest.java
+++ src/test/java/com/msuz/jcuesheet/TrackTest.java
@@ -11,65 +11,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Arrays;
 
 import com.msuz.jcuesheet.EnumConstants.*;
-import com.msuz.jcuesheet.Track;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class TrackTest {
-
-    private String[] dataTypeStrings = {
-        "AUDIO",
-        "CDG",
-        "MODE1/2048",
-        "MODE1/2352",
-        "MODE2/2048",
-        "MODE2/2324",
-        "MODE2/2336",
-        "MODE2/2352",
-        "CDI/2336",
-        "CDI/2352"
-    };
-    private String[] dataFormatStrings = {
-        "WAVE",
-        "MP3",
-        "AIFF",
-        "BINARY",
-        "MOTOROLA"
-    };
-
-    private static <T extends Enum<T>> T enumFromVal(Class<T> eenum, String value) {
-        for (T e : eenum.getEnumConstants()) {
-            if (e.name().equals(value))
-                return e;
-        }
-        return null;
-    }
-
+    // Convenience method
     private Track createTrack(DataType type, DataFormat format) {
         Track track = new Track(type, format);
         return track;
     }
 
-    @RepeatedTest(10)
     @DisplayName("Track constructor correctly sets all possible data types")
-    void testConstructorDataTypes() {
-        for (int i = 0; i < dataTypeStrings.length; ++i) {
-            assertEquals(dataTypeStrings[i],
-                createTrack(enumFromVal(DataType.class, dataTypeStrings[i]), DataFormat.WAVE).getDataType(),
-                "Type string should return from getDataType()");
-        }
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "AUDIO",
+        "CDG",
+        "MODE1_2048",
+        "MODE1_2352",
+        "MODE2_2048",
+        "MODE2_2324",
+        "MODE2_2336",
+        "MODE2_2352",
+        "CDI_2336",
+        "CDI_2352"
+    })
+    void testConstructorDataTypes(String dataType) {
+        Track testTrack = createTrack(Util.enumFromVal(DataType.class, dataType), DataFormat.WAVE);
+        assertEquals(dataType.replace('_','/'), testTrack.getDataType(), "Type string should return from getDataType()");
+        System.out.println("SUCCESS: " + dataType + " input produces " + testTrack.getDataType() + " output string");
     }
 
-    @RepeatedTest(5)
     @DisplayName("Track constructor correctly sets all possible data formats")
-    void testConstructorDataFormats() {
-        for (int i = 0; i < dataFormatStrings.length; ++i) {
-            assertEquals(dataFormatStrings[i],
-                createTrack(DataType.AUDIO, enumFromVal(DataFormat.class, dataFormatStrings[i])).getDataFormat(),
-                "Format string should return from getDataFormat()");
-        }
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "WAVE",
+        "MP3",
+        "AIFF",
+        "BINARY",
+        "MOTOROLA"
+    })
+    void testConstructorDataFormats(String dataFormat) {
+        Track testTrack = createTrack(DataType.AUDIO, Util.enumFromVal(DataFormat.class, dataFormat));
+        assertEquals(dataFormat, testTrack.getDataFormat(), "Format string should return from getDataFormat()");
+        System.out.println("SUCCESS: " + dataFormat + " input produces " + testTrack.getDataFormat() + " output string");
     }
     
 }
